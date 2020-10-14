36725e11bbd2ad4d94091dc78a8c837f30b78719
Lars Juul <ljb@easv.dk>
2020-10-14 11:18:02 +0200

    Adding Fluent Assettions

--- InnoTech.Core.PrimaryDriver.Adapters.Test/Services/LocationServiceTest.cs
+++ InnoTech.Core.PrimaryDriver.Adapters.Test/Services/LocationServiceTest.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using InnoTech.Core.Entity;
 using InnoTech.Core.InfratructurePorts.Repositories;
 using InnoTech.Core.PrimaryDriverAdapters.Services;
@@ -37,22 +38,12 @@ namespace InnoTech.Core.PrimaryDriver.Adapters.Test.Services
         {
             ILocationRepository locationRepository = new Mock<ILocationRepository>().Object;
             ILocationService locationService = new LocationService(locationRepository);
-            Assert.Throws<NullReferenceException>(() => locationService.Create(null as Location));
-        }
-        [Fact]
-        public void Create_WithLocationAsNull_ThrowsNullReferenceExceptionWithCorrectMessage()
-        {
-            var locationRepository = new Mock<ILocationRepository>().Object;
-            ILocationService locationService = new LocationService(locationRepository);
-            try
-            {
-                locationService.Create(null);
-                Assert.True(false, "We should never Get Here. Should Throw Exception");
-            }
-            catch (NullReferenceException e)
-            {
-                Assert.Equal("Location Cannot Be Null", e.Message);
-            }
+            Action action = () => locationService.Create(null as Location);
+            action.Should()
+                .Throw<NullReferenceException>()
+                .WithMessage("Location Cannot Be Null");
+            /*var exception = Assert.Throws<NullReferenceException>(() => locationService.Create(null as Location));
+            Assert.Equal("Location Cannot Be Null", exception.Message);*/
         }
         
         
